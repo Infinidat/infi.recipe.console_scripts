8346cde5c9e7a09d460ab3036de31565786bf26b
Guy Rozendorn <guy@rzn.co.il>
2012-08-29 20:39:18 +0300

    TRIVIAL initial work

---
+++ src/infi/recipe/console_scripts/make.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+PATH=C:\Program Files\Microsoft Visual Studio 9.0\VC;%PATH%
+PATH=C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC;%PATH%
+
+call VCVARSALL x86
+cl /c embed.c /I parts\python\include 
+link /out:embed-x86.exe embed.obj
+
+call VCVARSALL x86_amd64
+cl /c embed.c /I parts\python\include 
+link /out:embed-x64.exe embed.obj
+
