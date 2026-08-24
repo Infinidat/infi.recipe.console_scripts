19e5aaa49e6acd64f306e6cf713056aa3feec1b5
Alexander Deiter <adeiter@infinidat.com>
2026-08-24 19:05:06 +0400

    HOSTDEV-3730: Fix for Win32 error 126: The specified module could not be found
    Signed-off-by: Alexander Deiter <adeiter@infinidat.com>

--- src/infi/recipe/console_scripts/make.cmd
+++ src/infi/recipe/console_scripts/make.cmd
@@ -1,8 +1,31 @@
 @echo off
+setlocal
 
-PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build;%PATH%
+set "VCVARSALL=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\VCVARSALL.bat"
 
-call VCVARSALL x86_amd64
-cl /c embed3.c /I parts\python\include
-link /SUBSYSTEM:CONSOLE /out:embed3-x64.exe embed3.obj shell32.lib
-link /SUBSYSTEM:WINDOWS /out:embed3-gui-x64.exe embed3.obj shell32.lib
+call :build x86 x86
+if errorlevel 1 exit /b 1
+
+call :build x86_amd64 x64
+if errorlevel 1 exit /b 1
+
+exit /b 0
+
+:build
+setlocal
+call "%VCVARSALL%" %1
+if errorlevel 1 exit /b 1
+
+cl /nologo /O2 /W3 /c embed3.c /Foembed3-%2.obj
+if errorlevel 1 exit /b 1
+
+link /nologo /SUBSYSTEM:CONSOLE /out:embed3-%2.exe embed3-%2.obj shell32.lib
+if errorlevel 1 exit /b 1
+
+link /nologo /SUBSYSTEM:WINDOWS /out:embed3-gui-%2.exe embed3-%2.obj shell32.lib
+if errorlevel 1 exit /b 1
+
+del /q embed3-%2.obj
+if errorlevel 1 exit /b 1
+
+endlocal & exit /b 0
